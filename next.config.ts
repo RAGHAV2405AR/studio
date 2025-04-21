3f2ec51a2b35359943c168c6ccc4c7a5eebbed8f
RAGHAV2405AR <bhardwajraghav2405@gmail.com>
2025-04-21 10:25:36 +0530

    website name

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
+  
   },
+
+  devIndicators: {
+    buildActivity: false,
+  },
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
